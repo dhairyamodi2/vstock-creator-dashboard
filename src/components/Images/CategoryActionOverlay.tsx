bf4b6d3e3b1c1246f704060606db7284e1351a0d
dhairyam2002 <dhairyam2002@gmail.com>
2023-04-19 00:45:30 +0530

    add categories

---
+++ src/components/Images/CategoryActionOverlay.tsx
@@ -0,0 +1,90 @@
+import { getMyImages } from "@/redux/Images/images.actions";
+import { Category } from "@/redux/Images/images.types";
+import { OverlayProps } from "@/types/overlay"
+import { Box, Button, Heading, Input, List, ListItem, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Select, Text, UnorderedList } from "@chakra-ui/react"
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+interface CategoriesOverlay extends OverlayProps {
+    type: 'add' | 'remove';
+    id: string;
+}
+export const CategoryActionOverlay: React.FC<CategoriesOverlay> = function ({ isOpen, onOpen, onClose, type, id }) {
+    const dispatch = useDispatch();
+    const [categories, setCategories] = useState<Array<Category>>([]);
+    const [choosenCategory, setChoosenCategory] = useState<Category | string>("Select Category");
+    useEffect(() => {
+
+        async function getCategories() {
+            try {
+                const data = await fetch('http://localhost:3001/categories/all');
+                const res = await data.json();
+                if (res.success == true) {
+                    setCategories(res.data as Array<Category>)
+                }
+            } catch (error) {
+                console.log(error);
+            }
+
+        }
+        if (isOpen == true) {
+            getCategories();
+        }
+        setChoosenCategory('Select Category')
+    }, [isOpen])
+
+    async function handleAction() {
+        if (choosenCategory == 'Select Category' || typeof choosenCategory == 'string') {
+            alert("Please select category");
+            return;
+        }
+        alert(JSON.stringify({ id, category_name: choosenCategory.category_name, verdict: 'approved' }));
+        const data = await fetch('http://localhost:3001/stock/categories', {
+            method: type == 'add' ? 'PUT' : 'DELETE',
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: 'Bearer ' + localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                id,
+                category: {
+                    category_name: choosenCategory.category_name,
+                    verdict: 'approved'
+                }
+            })
+        })
+
+        if (data.status == 201) {
+            alert(type == 'add' ? "Category Added" : "Category Removed")
+            dispatch(getMyImages() as any);
+            return;
+        }
+        const res = await data.json();
+        alert(res.message);
+    }
+    return (
+        <Modal onClose={onClose} size={'lg'} isOpen={isOpen}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>
+                    <Heading size={'md'}>{type == 'add' ? "Add Categories" : "Remove Categories"}</Heading>
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <select value={typeof choosenCategory == 'string' ? choosenCategory : JSON.stringify(choosenCategory)} onChange={(e) => {
+                        const val = JSON.parse(e.target.value) as Category;
+                        setChoosenCategory(val)
+                    }}>
+                        <option value={'Select Category'}>Select Category</option>
+                        {categories.map((category) => {
+                            return <option value={JSON.stringify(category)}>{category.category_name}</option>
+                        })}
+                    </select>
+                    <Box display={'flex'} justifyContent={'center'}>
+                        <Button colorScheme={type == 'add' ? 'facebook' : 'red'} onClick={handleAction}>{type == 'add' ? "Add Categories" : "Remove Categories"}</Button>
+                    </Box>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    )
+}
--- src/components/Images/ImageInfo.tsx
+++ src/components/Images/ImageInfo.tsx
@@ -1,16 +1,19 @@
 import { Images, ImageState } from "@/redux/Images/images.types"
 import { State } from "@/redux/store"
-import { Badge, Tag, TagLabel } from "@chakra-ui/react"
+import { Badge, Button, Tag, TagLabel, useDisclosure } from "@chakra-ui/react"
+import { useState } from "react"
 import { useSelector } from "react-redux"
 import CustomLoader from "../Common/CustomLoader"
 import { CustomTag } from "../Common/Tags"
+import { CategoryActionOverlay } from "./CategoryActionOverlay"
 import ImageCard from "./ImageCard"
 
 interface ImageInfoProps{
     stock : Images
 }
 export const ImageInfo : React.FC<ImageInfoProps>= function ({stock}) {
-    
+    const [catActionType, setActionType] = useState<{type: 'add' | 'remove'}>({type: 'add'})
+    const {isOpen, onOpen, onClose} = useDisclosure();
     return (
         <div className="img-info-card">
             <Badge colorScheme={stock.verdict == 'approved' ? 'green' : stock.verdict == 'pending' ? 'orange' : 'red'} alignSelf={'flex-end'} padding={'5px'} fontSize={'medium'} fontWeight={'bold'}>{stock.verdict}</Badge>
@@ -33,6 +36,21 @@ export const ImageInfo : React.FC<ImageInfoProps>= function ({stock}) {
                             return <CustomTag key={category.category_name} text={category.category_name}/>
                         })}
                     </div>
+                    <div className="cat-actions">
+                        <Button colorScheme={'facebook'} onClick={() => {
+                            setActionType({
+                                type: 'add'
+                            })
+                            onOpen()
+                        }}>Add Categories</Button>
+                        <Button colorScheme={'red'} onClick={() => {
+                            setActionType({
+                                type: 'remove'
+                            })
+                            onOpen()
+                        }}>Remove Categories</Button>
+                    </div>
+                    <CategoryActionOverlay isOpen = {isOpen} onOpen = {onOpen} onClose = {onClose} type={catActionType.type} id={stock.id}/ >
                 </div>
             </div>
         </div>
