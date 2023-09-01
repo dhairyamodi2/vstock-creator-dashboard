d25a3f7ad350f67e8df05dfa221af3033c160f66
dhairyam2002 <dhairyam2002@gmail.com>
2023-09-01 17:22:57 +0530

    firebase config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,16 +1,6 @@
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDWKGz2GiJiYSaGxdqjqn5fAZdDCz0nNPs",
-  authDomain: "evervibe-6c846.firebaseapp.com",
-  projectId: "evervibe-6c846",
-  storageBucket: "evervibe-6c846.appspot.com",
-  messagingSenderId: "91094338467",
-  appId: "1:91094338467:web:5300cc2727f721547da816",
-  measurementId: "G-08L52RVW6W"
-};
-
+import { firebaseConfig } from "./firebaseConfig";
 
 export const app = initializeApp(firebaseConfig);
